b08bde360044ded3085b6ddd2bbd43c5f552f69b
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-14 10:56:23 +0100

    Adds class Education

---
+++ src/main/java/se/iths/helena/Education.java
@@ -0,0 +1,24 @@
+package se.iths.helena;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Education {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String Name;
+
+    public Education(){}
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
