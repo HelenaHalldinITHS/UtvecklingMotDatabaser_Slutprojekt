490f856f355ac5c4bc88fd6201583851a5b0cf04
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-15 11:15:48 +0100

    Adds code for methods in TeacherDaoImpl

--- src/main/java/se/iths/helena/impl/TeacherDaoImpl.java
+++ src/main/java/se/iths/helena/impl/TeacherDaoImpl.java
@@ -11,31 +11,37 @@ public class TeacherDaoImpl extends DaoImpl implements TeacherDao {
 
     @Override
     public void add(Teacher teacher) {
-
+        getEntityManager().getTransaction().begin();
+        getEntityManager().persist(teacher);
+        getEntityManager().getTransaction().commit();
     }
 
     @Override
     public void update(Teacher teacher) {
-
+        getEntityManager().getTransaction().begin();
+        getEntityManager().merge(teacher);
+        getEntityManager().getTransaction().commit();
     }
 
     @Override
     public void showInfo(Teacher teacher) {
-
+        teacher.print();
     }
 
     @Override
     public Teacher getById(int id) {
-        return null;
+        return getEntityManager().find(Teacher.class, id);
     }
 
     @Override
     public void showAll() {
-
+        getEntityManager().createQuery("SELECT t FROM Teacher t", Teacher.class).getResultList().forEach(Teacher::print);
     }
 
     @Override
     public void delete(Teacher teacher) {
-
+        getEntityManager().getTransaction().begin();
+        getEntityManager().remove(teacher);
+        getEntityManager().getTransaction().commit();
     }
 }
