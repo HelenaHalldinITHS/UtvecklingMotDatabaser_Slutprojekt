9470b54dadf23bc1b653e3e6a7f52db5ef29834d
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-15 10:18:50 +0100

    Changes fields name from "Name" to "name"

--- src/main/java/se/iths/helena/entities/Education.java
+++ src/main/java/se/iths/helena/entities/Education.java
@@ -10,25 +10,25 @@ public class Education {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String Name;
+    private String name;
 
     public Education() {
     }
 
     public Education(String name) {
-        Name = name;
+        this.name = name;
     }
 
     public String getName() {
-        return Name;
+        return name;
     }
 
     public void setName(String name) {
-        Name = name;
+        this.name = name;
     }
 
     public void print(){
         System.out.println("id:" + id +
-                    ", Name:" + Name);
+                    ", name:" + name);
     }
 }
