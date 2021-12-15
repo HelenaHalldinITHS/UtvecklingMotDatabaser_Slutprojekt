4940b336b1640a6026748e44e0cdbef039a3d897
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-15 10:52:36 +0100

    Makes StudentDaoImpl extend DaoImpl

---
+++ src/main/java/se/iths/helena/impl/StudentDaoImpl.java
@@ -0,0 +1,40 @@
+package se.iths.helena.impl;
+
+import se.iths.helena.dao.StudentDao;
+import se.iths.helena.entities.Student;
+
+public class StudentDaoImpl extends DaoImpl implements StudentDao  {
+
+    public StudentDaoImpl(){
+        super();
+    }
+    @Override
+    public void add(Student student) {
+
+    }
+
+    @Override
+    public void update(Student student) {
+
+    }
+
+    @Override
+    public void showInfo(Student student) {
+
+    }
+
+    @Override
+    public Student getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void showAll() {
+
+    }
+
+    @Override
+    public void delete(Student student) {
+
+    }
+}
