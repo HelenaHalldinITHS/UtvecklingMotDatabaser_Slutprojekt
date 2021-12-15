bf08310878487ac6ccffa5a7f65c954993103d0a
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-15 11:13:57 +0100

    Adds Class TeacherDaoImpl

---
+++ src/main/java/se/iths/helena/impl/TeacherDaoImpl.java
@@ -0,0 +1,41 @@
+package se.iths.helena.impl;
+
+import se.iths.helena.dao.TeacherDao;
+import se.iths.helena.entities.Teacher;
+
+public class TeacherDaoImpl extends DaoImpl implements TeacherDao {
+
+    public TeacherDaoImpl(){
+        super();
+    }
+
+    @Override
+    public void add(Teacher teacher) {
+
+    }
+
+    @Override
+    public void update(Teacher teacher) {
+
+    }
+
+    @Override
+    public void showInfo(Teacher teacher) {
+
+    }
+
+    @Override
+    public Teacher getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void showAll() {
+
+    }
+
+    @Override
+    public void delete(Teacher teacher) {
+
+    }
+}
