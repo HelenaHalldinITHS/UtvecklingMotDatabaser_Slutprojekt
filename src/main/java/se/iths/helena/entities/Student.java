d850c18bfc73f3f10865706f1da50eb7a14e97c6
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-15 08:40:01 +0100

    Adds getters/setters in Student

--- src/main/java/se/iths/helena/entities/Student.java
+++ src/main/java/se/iths/helena/entities/Student.java
@@ -14,11 +14,41 @@ public class Student {
     @ManyToOne
     private Education education;
 
-    public Student(){}
+    public Student() {
+    }
 
     public Student(String firstName, String lastName, Education education) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.education = education;
+    }
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Education getEducation() {
+        return education;
+    }
+
+    public void setEducation(Education education) {
         this.education = education;
     }
 }
