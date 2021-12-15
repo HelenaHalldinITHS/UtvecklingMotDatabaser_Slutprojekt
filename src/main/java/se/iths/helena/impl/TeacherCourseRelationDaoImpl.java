51b128c33da9dfc59abd23de576607cc60b82636
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-15 11:27:50 +0100

    Adds code for method add in TeacherCourseRelationDaoImpl

--- src/main/java/se/iths/helena/impl/TeacherCourseRelationDaoImpl.java
+++ src/main/java/se/iths/helena/impl/TeacherCourseRelationDaoImpl.java
@@ -3,16 +3,20 @@ package se.iths.helena.impl;
 import se.iths.helena.dao.TeacherCourseRelationDao;
 import se.iths.helena.entities.Course;
 import se.iths.helena.entities.Teacher;
+import se.iths.helena.entities.TeacherCourseRelation;
 
 public class TeacherCourseRelationDaoImpl extends DaoImpl implements TeacherCourseRelationDao {
 
-    TeacherCourseRelationDaoImpl(){
+    public TeacherCourseRelationDaoImpl(){
         super();
     }
 
     @Override
     public void add(Teacher teacher, Course course) {
-
+        TeacherCourseRelation relation = new TeacherCourseRelation(teacher,course);
+        getEntityManager().getTransaction().begin();
+        getEntityManager().persist(relation);
+        getEntityManager().getTransaction().commit();
     }
 
     @Override
