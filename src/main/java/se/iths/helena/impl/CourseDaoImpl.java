66fd3d44b72b0b30b248b44d267e1e4e26db79e1
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-15 10:15:54 +0100

    Adds Class CourseDaoImpl

---
+++ src/main/java/se/iths/helena/impl/CourseDaoImpl.java
@@ -0,0 +1,48 @@
+package se.iths.helena.impl;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import se.iths.helena.dao.CourseDao;
+import se.iths.helena.entities.Course;
+
+public class CourseDaoImpl implements CourseDao {
+    private EntityManagerFactory emf;
+    private EntityManager em;
+
+    CourseDaoImpl(){
+        emf = Persistence.createEntityManagerFactory("jpa");
+        em = emf.createEntityManager();
+    }
+
+
+    @Override
+    public void add(Course course) {
+
+    }
+
+    @Override
+    public void update(Course course) {
+
+    }
+
+    @Override
+    public void showInfo(Course course) {
+
+    }
+
+    @Override
+    public Course getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void showAll() {
+
+    }
+
+    @Override
+    public void delete(Course course) {
+
+    }
+}
