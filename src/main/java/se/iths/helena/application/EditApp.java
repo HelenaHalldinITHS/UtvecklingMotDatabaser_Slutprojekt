67efc8113e7c75b9aeae674964da69e7acbb9f13
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-17 13:14:03 +0100

    Adds menu in EditApp

--- src/main/java/se/iths/helena/application/EditApp.java
+++ src/main/java/se/iths/helena/application/EditApp.java
@@ -1,8 +1,57 @@
 package se.iths.helena.application;
 
+import se.iths.helena.dao.CourseDao;
+import se.iths.helena.dao.EducationDao;
+import se.iths.helena.dao.StudentDao;
+import se.iths.helena.dao.TeacherDao;
+import se.iths.helena.impl.CourseDaoImpl;
+import se.iths.helena.impl.EducationDaoImpl;
+import se.iths.helena.impl.StudentDaoImpl;
+import se.iths.helena.impl.TeacherDaoImpl;
+
 public class EditApp {
+    EducationDao educationDao;
+    CourseDao courseDao;
+    StudentDao studentDao;
+    TeacherDao teacherDao;
+
+    public EditApp() {
+        educationDao = new EducationDaoImpl();
+        courseDao = new CourseDaoImpl();
+        studentDao = new StudentDaoImpl();
+        teacherDao = new TeacherDaoImpl();
+    }
 
     public void run(){
+        do {
+            printMenu();
+            int choice = getChoice();
+            if (choice == 0)
+                break;
+            else
+                runChoice(choice);
+        } while (true);
+    }
+
+
+
+    private void printMenu() {
+        System.out.println("""
+                What would you like to edit?
+                Choose one of the following by writing its corresponding number:
+                1. Educations
+                2. Courses
+                3. Students
+                4. Teachers
+                0. Exit
+                """);
+    }
+
+    private int getChoice() {
+        return InputHandler.getIntegerInput(0, 4);
+    }
+
+    private void runChoice(int choice) {
 
     }
 }
